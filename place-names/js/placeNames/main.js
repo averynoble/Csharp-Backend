f11d7bba56b03fa70ef261bde8e5824f0c1b785e
averynoble <averynoble.an@gmail.com>
2021-08-23 11:43:38 -0500

    translated place names to js

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -5,8 +5,15 @@ const names = [
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
+  console.log(name);
+}
+
+const theName = names.filter(n => n.startsWith("The"))
+console.log(`${theName}`);
+
+for (const name of theName) {
   console.log(name);
 }
