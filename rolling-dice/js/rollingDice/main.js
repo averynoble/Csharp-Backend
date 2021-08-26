14833b672d6f3e03350669ad6d138f178dd9533a
averynoble <averynoble.an@gmail.com>
2021-08-26 16:12:08 -0500

    fixed WishList and converted to c#

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,42 @@
-// Put your code here
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+const Roll = (min, max) => {
+    min = Math.ceil(1)
+    max = Math.floor(6)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+for (let i = 0; i < 10; i++) {
+    let die1 = Roll()
+    let die2 = Roll()
+
+    let message = `${die1} + ${die2} == ${die1 + die2}`.toString()
+
+    if (die1 === die2) {
+        message += " DOUBLES!"
+    }
+    console.log(message)
+
+    switch (message) {
+    
+        case 1:
+            dieString = "one";
+            break;
+        case 2: 
+            dieString = "two";
+            break;
+        case 3: 
+            dieString = "three";
+            break;
+        case 4:
+            dieString = "four";
+            break;
+        case 5:
+            dieString = "five";
+            break;
+        case 6:
+            dieString = "six";
+            break;
+    }
+}
---
+++ wish-list/csharp/wish.cs
@@ -0,0 +1,19 @@
+using System;
+
+void Main() 
+{
+    List<Wish> IWishFor = new List<Wish>
+    {
+        List<string> = new List<string> {"An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'"},
+        List<string> = new List<string> {"The ability to think of interesting wishes on short notice"},
+        List<string> = new List<string> {"A washtub of Skittles"},
+        List<string> = new List<string> {"World peace, but not the kind of world peach where all the humans are removed from existence"},
+        List<string> = new List<string> {"A TV that is NOT internet-capable"},
+        List<string> = new List<string> {"The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?"}, 
+    };
+
+    foreach (Wish IWishFor in Wish)
+    {
+        Console.WriteLine($"My wishes... {Wish}");
+    }
+}
